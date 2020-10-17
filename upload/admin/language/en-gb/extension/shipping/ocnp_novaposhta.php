0704feaaba2a81d7a48e2983ee4a4ede6c110033
SergeyGadzhilov <sgaddreams@gmail.com>
2020-10-17 10:07:07 +0300

    Fixed #7 Add tooltips with links to official documentation.
    * Tab support has been added.
    
    * Content for support tab
    
    * FIxed #7 Add tooltips with links to official documentation.
    
    Co-authored-by: Serhii Hadzhilov <gadzhilov.sergey@gmail.com>

--- upload/admin/language/en-gb/extension/shipping/ocnp_novaposhta.php
+++ upload/admin/language/en-gb/extension/shipping/ocnp_novaposhta.php
@@ -13,6 +13,8 @@ $_['ocnp_text_sync_success']  = 'Synchronization has been finished successfully!
 $_['ocnp_text_contacts_label'] = 'Contacts';
 $_['ocnp_text_contacts_text'] = 'For all questions related to the add-on, please contact:';
 
+$_['ocnp_api_tab_key_link'] = 'The API key can be generated in you account: <a href=\'https://my.novaposhta.ua/settings/index#apikeys\'>API Key</a>';
+$_['ocnp_api_tab_key_instruction'] = 'Detailed instruction how to generate the API key you can find here: <a href=\'https://devcenter.novaposhta.ua/blog/%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5-api-%D0%BA%D0%BB%D1%8E%D1%87%D0%B0\'>Instruction</a>';
 
 /* Sync table on Syncronization tab */
 $_['ocnp_text_sync_table_type']  = 'Name';
@@ -22,13 +24,13 @@ $_['ocnp_text_sync_table_desc']  = 'Description';
 $_['ocnp_text_sync_table_sync']  = 'Synchronize';
 
 $_['ocnp_text_sync_table_cities']  = 'Cities';
-$_['ocnp_text_sync_table_cities_desc']  = 'Sync cities to which delivery by the postal company is possible';
+$_['ocnp_text_sync_table_cities_desc']  = 'Sync cities to which delivery by the postal company is possible.<br>(It is recommended to update once a day)';
 
 $_['ocnp_text_sync_table_areas']  = 'Areas';
-$_['ocnp_text_sync_table_areas_desc']  = 'Sync areas to which delivery by the postal company is possible';
+$_['ocnp_text_sync_table_areas_desc']  = 'Sync areas to which delivery by the postal company is possible.<br>(It is recommended to update once a month)';
 
 $_['ocnp_text_sync_table_warehouses']  = 'Warehouses';
-$_['ocnp_text_sync_table_warehouses_desc']  = 'Sync warehouses';
+$_['ocnp_text_sync_table_warehouses_desc']  = 'Sync warehouses.<br>(It is recommended to update once a day)';
 
 /* Entry */
 $_['entry_status']     = 'Status:';
